2be584b6e81374b951224dc505d5ceeb655e675f
Thomas Galliker <thomas@bluewin.ch>
2021-10-28 19:12:54 +0200

    Display car template embedded as well as as external xaml file

--- 1.5-DataBinding/DatabindingDemos/AutoBinding/AutoBinding2/Views/MainWindow.xaml.cs
+++ 1.5-DataBinding/DatabindingDemos/AutoBinding/AutoBinding2/Views/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
 using AutoBinding2.Model;
 
@@ -10,24 +9,25 @@ namespace AutoBinding2
         public MainWindow()
         {
             this.InitializeComponent();
-            this.DataContext = new List<Car>
+            var carsList = new List<Car>
             {
                 new Car
                 {
-                    Image = "Images\\Audi-A7-Sportback.jpg",
+                    Image = "..\\Images\\Audi-A7-Sportback.jpg",
                     Model = "Audi A7 Sportback"
                 },
                 new Car
                 {
-                    Image = "Images\\BMW-6er-Gran-Coupé.jpg",
+                    Image = "..\\Images\\BMW-6er-Gran-Coupé.jpg",
                     Model = "BMW 6er Gran Coupé"
                 },
                 new Car
                 {
-                    Image = "Images\\Mercedes-CLS.jpg",
+                    Image = "..\\Images\\Mercedes-CLS.jpg",
                     Model = "Mercedes CLS"
                 }
             };
+            this.DataContext = carsList;
         }
     }
 }
