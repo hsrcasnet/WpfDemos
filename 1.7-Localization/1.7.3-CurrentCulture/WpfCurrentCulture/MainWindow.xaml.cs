e02ac9aafb2d667e0bc86f8e5d58488ef70cd7c7
Thomas Galliker <thomas@bluewin.ch>
2021-10-28 18:27:59 +0200

    Add demo app for currentculture

---
+++ 1.7-Localization/1.7.3-CurrentCulture/WpfCurrentCulture/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using WpfCurrentCulture.ViewModels;
+
+namespace WpfCurrentCulture
+{
+    public partial class MainWindow : Window
+    {
+        private readonly CultureViewModel viewModel;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+
+            this.viewModel = new CultureViewModel();
+            this.DataContext = this.viewModel;
+        }
+
+        private void ResetCurrentCulture(object sender, RoutedEventArgs e)
+        {
+            this.viewModel.ResetCurrentCulture();
+        }
+
+        private void ResetCurrentUICulture(object sender, RoutedEventArgs e)
+        {
+            this.viewModel.ResetCurrentUICulture();
+        }
+    }
+}
---
+++ 1.7-Localization/1.7.3-CurrentCulture/WpfCurrentCulture/ViewModels/CultureViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace WpfCurrentCulture.ViewModels
+{
+    public class CultureViewModel : ViewModelBase
+    {
+        private readonly CultureInfo originalCurrentCulture;
+        private readonly CultureInfo originalCurrentUICulture;
+
+        private CultureInfo currentCulture;
+        private CultureInfo currentUiCulture;
+
+        public CultureViewModel()
+        {
+            this.originalCurrentCulture = Thread.CurrentThread.CurrentCulture;
+            this.originalCurrentUICulture = Thread.CurrentThread.CurrentUICulture;
+
+            this.CurrentCulture = Thread.CurrentThread.CurrentCulture;
+            this.CurrentUICulture = Thread.CurrentThread.CurrentUICulture;
+        }
+
+        public CultureInfo CurrentCulture
+        {
+            get => this.currentCulture;
+            set
+            {
+                if (this.currentCulture != value)
+                {
+                    this.currentCulture = value;
+                    this.OnPropertyChanged(nameof(this.CurrentCulture));
+                }
+            }
+        }
+
+        public CultureInfo CurrentUICulture
+        {
+            get => this.currentUiCulture;
+            set
+            {
+                if (this.currentUiCulture != value)
+                {
+                    this.currentUiCulture = value;
+                    this.OnPropertyChanged(nameof(this.CurrentUICulture));
+                }
+            }
+        }
+
+        public void ResetCurrentCulture()
+        {
+            this.CurrentCulture = this.originalCurrentCulture;
+        }
+
+        public void ResetCurrentUICulture()
+        {
+            this.CurrentUICulture = this.originalCurrentUICulture;
+        }
+    }
+}
