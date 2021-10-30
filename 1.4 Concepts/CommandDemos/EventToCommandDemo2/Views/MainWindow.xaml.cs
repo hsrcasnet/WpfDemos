afde73db00a4c1813cb8b0bbcdc8bd52e47fff53
Thomas Galliker <thomas@bluewin.ch>
2021-10-30 08:53:16 +0200

    Add event to command demo 2: MouseBinding

---
+++ 1.4 Concepts/CommandDemos/EventToCommandDemo2/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using EventToCommandDemo2.ViewModels;
+
+namespace EventToCommandDemo2.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.DataContext = new KitchenViewModel();
+        }
+    }
+}
