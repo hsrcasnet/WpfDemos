78e82a3bff171418eae136389b1a10472f386788
Thomas Galliker <thomas@bluewin.ch>
2021-11-01 07:35:22 +0100

    Re-add Xliff resources; restore MAT capability after Target Framework Update

--- 1.7-Localization/1.7.2-WpfMultilingual/WpfMultilingual/Properties/Resources.Designer.cs
+++ 1.7-Localization/1.7.2-WpfMultilingual/WpfMultilingual/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace WpfMultilingual.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -61,11 +61,20 @@ namespace WpfMultilingual.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Hello World!.
+        ///   Looks up a localized string similar to Hello World.
         /// </summary>
-        public static string HelloWorld {
+        public static string HelloWorldLabelText {
             get {
-                return ResourceManager.GetString("HelloWorld", resourceCulture);
+                return ResourceManager.GetString("HelloWorldLabelText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to WPF Multilingual Demo.
+        /// </summary>
+        public static string MainWindowPageTitle {
+            get {
+                return ResourceManager.GetString("MainWindowPageTitle", resourceCulture);
             }
         }
     }
