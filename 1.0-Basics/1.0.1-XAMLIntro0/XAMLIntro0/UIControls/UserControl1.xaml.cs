ef859783dc1a989975a9130ad64324072117aeae
Thomas Galliker <thomas@bluewin.ch>
2022-10-18 06:44:14 +0200

    Add namespace examples

---
+++ 1.0-Basics/1.0.1-XAMLIntro0/XAMLIntro0/UIControls/UserControl1.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MyCompany.UIControls
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
