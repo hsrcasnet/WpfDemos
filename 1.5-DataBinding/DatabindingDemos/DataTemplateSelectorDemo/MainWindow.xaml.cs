274aef2cbdf33ec575a30aa1273baba00e5c8c2f
Thomas Galliker <thomas@bluewin.ch>
2022-10-19 17:19:40 +0200

    Cleanup

--- 1.5-DataBinding/DatabindingDemos/DataTemplateSelectorDemo/MainWindow.xaml.cs
+++ 1.5-DataBinding/DatabindingDemos/DataTemplateSelectorDemo/MainWindow.xaml.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 
 namespace DataTemplateSelectorDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
