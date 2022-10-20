7b40b1c30ab7408fcfd1b06200f9f80371cff3c9
Thomas Galliker <thomas@bluewin.ch>
2022-10-20 17:07:23 +0200

    Extend sample to become a small notepad editor

--- 1.4 Concepts/CommandDemos/CommandDemoApplicationCommands/MainWindow.xaml.cs
+++ 1.4 Concepts/CommandDemos/CommandDemoApplicationCommands/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Microsoft.Win32;
 
 namespace CommandDemoApplicationCommands
 {
     public partial class MainWindow : Window
     {
+        private const string TextFilesFilter = "Text files (*.txt)|*.txt";
         private bool isDirty = false;
+        private string currentFileName;
 
         public MainWindow()
         {
@@ -30,20 +35,50 @@ namespace CommandDemoApplicationCommands
 
         private void NewCommand(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show($"New command triggered from Source={e.Source.GetType().Name}");
+            Debug.WriteLine($"New command triggered from Source={e.Source.GetType().Name}");
+            this.currentFileName = null;
+            this.Notepad.Text = string.Empty;
             this.isDirty = false;
         }
 
         private void OpenCommand(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show($"Open command triggered from Source={e.Source.GetType().Name}");
-            this.isDirty = false;
+            Debug.WriteLine($"Open command triggered from Source={e.Source.GetType().Name}");
+
+            // Demo: Simple use case of OpenFileDialog
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = TextFilesFilter;
+            var result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                this.currentFileName = openFileDialog.FileName;
+                this.Notepad.Text = File.ReadAllText(openFileDialog.FileName);
+                this.isDirty = false;
+            }
         }
 
         private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show($"Save command triggered from Source={e.Source.GetType().Name}");
-            this.isDirty = false;
+            Debug.WriteLine($"Save command triggered from Source={e.Source.GetType().Name}");
+
+            if (currentFileName == null)
+            {
+                // Demo: Simple use case of SaveFileDialog
+                var saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = TextFilesFilter;
+                var result = saveFileDialog.ShowDialog();
+                if (result == true)
+                {
+                    this.currentFileName = saveFileDialog.FileName;
+                    File.WriteAllText(saveFileDialog.FileName, this.Notepad.Text);
+                    this.isDirty = false;
+                }
+            }
+            else
+            {
+                File.WriteAllText(this.currentFileName, this.Notepad.Text);
+                this.isDirty = false;
+            }
         }
 
         private void OnTextBoxTextChanged(object sender, TextChangedEventArgs e)
@@ -55,6 +90,5 @@ namespace CommandDemoApplicationCommands
         {
             e.CanExecute = this.isDirty;
         }
-
     }
 }
