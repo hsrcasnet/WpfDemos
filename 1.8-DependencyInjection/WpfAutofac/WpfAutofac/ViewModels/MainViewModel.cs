6aa6107150317a296df07c9cae2fc150a9672b11
Thomas Galliker <thomas@bluewin.ch>
2022-10-20 18:39:13 +0200

    Migrate from MvvmLight to .NET Community Toolkit

--- 1.8-DependencyInjection/WpfAutofac/WpfAutofac/ViewModels/MainViewModel.cs
+++ 1.8-DependencyInjection/WpfAutofac/WpfAutofac/ViewModels/MainViewModel.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Threading.Tasks;
-using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using WpfAutofac.Services;
 
 namespace WpfAutofac.ViewModels
 {
-    public sealed class MainViewModel : ViewModelBase, IMainViewModel
+    public sealed class MainViewModel : ObservableObject, IMainViewModel
     {
         private readonly IMessageService messageService;
         private readonly ILoadingIndicatorService loadingIndicatorService;
@@ -20,30 +20,17 @@ namespace WpfAutofac.ViewModels
             this.loadingIndicatorService = loadingIndicatorService;
         }
 
-        public string ViewTitle
-        {
-            get { return "Welcome to WPF+Autofac"; }
-        }
+        public string ViewTitle => "Welcome to WPF+Autofac";
 
         public string FirstName
         {
             get => this.firstName;
-            set
-            {
-                this.firstName = value;
-                this.RaisePropertyChanged(nameof(this.FirstName));
-            }
+            set => this.SetProperty(ref this.firstName, value);
         }
 
-        public RelayCommand ShowMessageBoxCommand
-        {
-            get
-            {
-                return this.showMessageBoxCommand ?? (this.showMessageBoxCommand = new RelayCommand(
+        public RelayCommand ShowMessageBoxCommand => this.showMessageBoxCommand ??= new RelayCommand(
                            execute: async () => await this.ShowMessageBox(),
-                           canExecute: () => !this.IsLoading));
-            }
-        }
+                           canExecute: () => !this.IsLoading);
 
         private async Task ShowMessageBox()
         {
@@ -52,7 +39,7 @@ namespace WpfAutofac.ViewModels
                 this.IsLoading = true;
 
                 this.loadingIndicatorService.ShowLoadingIndicator();
-                
+
                 // Call services here...
                 await Task.Delay(2000);
 
@@ -61,7 +48,7 @@ namespace WpfAutofac.ViewModels
                 // Present the result
                 this.messageService.Show($"FirstName={this.FirstName}");
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 this.messageService.Show($"Something went wrong. Try again. [Error Code 1002]");
             }
